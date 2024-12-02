4b769bdf28e8a36cc4b907aeaf9266243a030b9d
aldisiomedeiros-lab <53276090+aldisiomedeiros-lab@users.noreply.github.com>
2024-12-02 20:54:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // app.js
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
+
 const listaTarefas = document.getElementById('lista-tarefas');
 
 function adicionarTarefa() {
